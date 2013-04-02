8a9766970c72596de34086ff68b936edc4b609b9
Dillon Young <info@dillonyoung.ca>
2013-04-02 10:45:22 -0400

    Initial Commit
    - Started building the layouts
    - Start the database code

---
+++ TraCare/src/ca/dillonyoung/tracare/Locations.java
@@ -0,0 +1,29 @@
+package ca.dillonyoung.tracare;
+
+public class Locations {
+
+	private int id;
+	private float latitude;
+	private float longitude;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public float getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(float latitude) {
+		this.latitude = latitude;
+	}
+	public float getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(float longitude) {
+		this.longitude = longitude;
+	}
+	
+	
+}
